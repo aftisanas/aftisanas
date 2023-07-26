449f33d53478e111e1eecc57e94b5f73aa9433a1
Anas AFTIS <afanas650@gmail.com>
2023-07-26 01:30:18 +0100

    contact form / phpmailer

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,53 +1,62 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require './vendor/autoload.php';
+
+
 // configure
-$from = 'Contact form contact@anas-aftis.com';
-$sendTo = 'contact@anas-aftis.com';
+// $from = 'Contact form contact@anas-aftis.com';
+// $sendTo = 'contact@anas-aftis.com';
 $subject = 'New message from contact form';
 $fields = array('name' => $_POST['name'], 'email' => $_POST['email'], 'message' => $_POST['message']); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 // let's do the sending
-
 if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
-    
     //your site secret key
     $secret = '6Lc56wUnAAAAAL0gnAzyuPTcsPgrgAfBANS6FyZx';
-    
-    //get verify response data
 
+    //get verify response data
     $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
     $verifyResponse = curl_exec($c);
 
     $responseData = json_decode($verifyResponse);
     if($responseData->success):
-
+        $mail = new PHPMailer(true);
         try
         {
-            $emailText = nl2br("You have new message from Contact Form\n");
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->isSMTP();
+            $mail->Host       = 'smtppro.zoho.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'contact@anas-aftis.com';
+            $mail->Password   = 'secret';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port       = 465;
 
-            foreach ($_POST as $key => $value) {
+            $mail->addAddress($_POST['email'], $_POST['name']);
 
-                if (isset($fields[$key])) {
-                    $emailText .= nl2br("$fields[$key]: $value\n");
-                }
-            }
-
-            $headers = array('Content-Type: text/html; charset="UTF-8";',
-                'From: ' . $from,
-                'Reply-To: ' . $from,
-                'Return-Path: ' . $from,
-            );
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $_POST['message'];
             
-            mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
+            $mail->send();
+            // echo 'Message has been sent';
+            
+            // mail($sendTo, $subject, $emailText, implode("\n", $headers));
             $responseArray = array('type' => 'success', 'message' => $okMessage);
         }
-        catch (\Exception $e)
+        catch (Exception $e)
         {
             $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+            echo $mail->ErrorInfo;
         }
 
         if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
@@ -75,8 +84,8 @@ else:
     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
     $encoded = json_encode($responseArray);
 
-            header('Content-Type: application/json');
+    header('Content-Type: application/json');
 
-            echo $encoded;
+    echo $encoded;
 endif;
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a4f0e81e2e2f5688e7380b3e03e2f4d39c4afe10',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a4f0e81e2e2f5688e7380b3e03e2f4d39c4afe10',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
